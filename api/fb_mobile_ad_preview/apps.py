ab2e7a8e67a8410d1add22949ec0af124e21a14e
xinrwu <xinr.wu@mail.utoronto.ca>
2017-04-21 15:14:23 -0400

    basic django setup

---
+++ api/fb_mobile_ad_preview/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FbMobileAdPreviewConfig(AppConfig):
+    name = 'fb_mobile_ad_preview'
